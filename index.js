4795765203abe316cec25d9d923ae28278fa8741
shahrukh <mohiuddinshahrukh@gmail.com>
2023-10-10 15:37:33 +0500

    try{} catch(){} added

--- index.js
+++ index.js
@@ -7,41 +7,66 @@ const io = require("socket.io")(8900, {
 let users = [];
 
 const addUser = (userId, socketId) => {
-  !users.some((user) => user.userId === userId) &&
-    users.push({
-      userId,
-      socketId,
-    });
+  try {
+    console.log(userId);
+    console.log(socketId);
+    !users.some((user) => user.userId === userId) &&
+      users.push({
+        userId,
+        socketId,
+      });
+  } catch (error) {
+    console.log(`${error}`);
+  }
 };
 const removeUser = (socketId) => {
-  users = users.filter((user) => {
-    user.socketId !== socketId;
-  });
+  try {
+    users = users.filter((user) => {
+      user.socketId !== socketId;
+    });
+  } catch (error) {
+    console.log(`${error}`);
+  }
 };
 
 const getUser = (userId) => {
-  return users.find((user) => user.userId === userId);
+  try {
+    return users.find((user) => user.userId === userId);
+  } catch (error) {
+    console.log(`${error}`);
+  }
 };
 
 io.on("connection", (socket) => {
   console.log("A user connected to socket server");
 
   socket.on("addUser", (userId) => {
-    addUser(userId, socket.id);
-    io.emit("getUsers", users);
+    try {
+      addUser(userId, socket.id);
+      io.emit("getUsers", users);
+    } catch (error) {
+      console.log(`${error}`);
+    }
   });
 
   socket.on("disconnect", () => {
-    console.log("A user has disconnected");
-    removeUser(socket.id);
-    io.emit("getUsers", users);
+    try {
+      console.log("A user has disconnected");
+      removeUser(socket.id);
+      io.emit("getUsers", users);
+    } catch (error) {}
   });
 
   socket.on("sendMessage", ({ senderId, receiverId, text }) => {
-    const user = getUser(receiverId);
-    io.to(user.socketId).emit("getMessage", {
-      senderId,
-      text,
-    });
+    try {
+      const user = getUser(receiverId);
+      console.log("User:", user);
+      io.to(user.socketId).emit("getMessage", {
+        senderId,
+        text,
+      });
+    } catch (error) {
+      console.log(`${error}`);
+    }
   });
 });
